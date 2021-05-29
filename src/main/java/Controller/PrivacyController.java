e1e1f35e205bc2862b27c1c990d60297e89efd91
bomin <dlwoqur159@naver.com>
2021-05-30 00:07:21 +0900

    action 추가

--- src/main/java/Controller/PrivacyController.java
+++ src/main/java/Controller/PrivacyController.java
@@ -4,10 +4,14 @@ import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import vo.ActionForward;
+import action.Action;
 
+@WebServlet("*.do")
 public class PrivacyController extends HttpServlet {
 
 	@Override
@@ -21,6 +25,14 @@ public class PrivacyController extends HttpServlet {
 	}
 	
 	public void process(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("UTF-8");
+		String RequestURI=req.getRequestURI();
+		String contextPath=req.getContextPath();
+		String command=RequestURI.substring(contextPath.length());
+		ActionForward forward= null;
+		Action action=null;
+		
+		
 		RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/index.jsp");
 		rd.forward(req, resp);
 	}
